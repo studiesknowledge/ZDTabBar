0e255fd6461403dd1b3e5c0ce4195801ddae97eb
赵东亮 <3012287667@qq.com>
2016-11-15 14:14:18 +0800

    2. 创建tabBarController
    1. 创建tabBarController
    2. 设置根控制器为tabBarcontroller

---
+++ ZDTabBar/ZDTabBarViewController.m
@@ -0,0 +1,38 @@
+//
+//  ZDTabBarViewController.m
+//  ZDTabBar
+//
+//  Created by 赵东亮 on 16/11/15.
+//  Copyright © 2016年 赵东亮. All rights reserved.
+//
+
+#import "ZDTabBarViewController.h"
+
+@interface ZDTabBarViewController ()
+
+@end
+
+@implementation ZDTabBarViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
