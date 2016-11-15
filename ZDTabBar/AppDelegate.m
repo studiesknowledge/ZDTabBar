75dbba26e529c76d88813a1a7d9ce1ffcea52c4a
赵东亮 <3012287667@qq.com>
2016-11-15 14:14:42 +0800

    3.设置根控制器

--- ZDTabBar/AppDelegate.m
+++ ZDTabBar/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ZDTabBarViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,15 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    
+    self.window.rootViewController = [[ZDTabBarViewController alloc] init];
+    
+    [self.window makeKeyAndVisible];
+    
+    
     return YES;
 }
 
