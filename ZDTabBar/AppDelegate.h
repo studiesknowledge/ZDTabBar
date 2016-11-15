a2cf60ed3fc67aa453fa3c78874f526be9c8690f
赵东亮 <3012287667@qq.com>
2016-11-15 14:09:40 +0800

    1. 创建工程

---
+++ ZDTabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZDTabBar
+//
+//  Created by 赵东亮 on 16/11/15.
+//  Copyright © 2016年 赵东亮. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZDTabBar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ZDTabBar
+//
+//  Created by 赵东亮 on 16/11/15.
+//  Copyright © 2016年 赵东亮. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
